025f57e4caf4f6f4d3d15ec080a325c4d27e8d82
Nasir Aftab <nasiraftab123@gmail.com>
2025-04-12 01:24:17 +0530

    New Update

--- src/pages/job-placement/Nursing-In-Germany/data.js
+++ src/pages/job-placement/Nursing-In-Germany/data.js
@@ -5,6 +5,11 @@ import Step4 from "../../../assets/imgs/job-placement/step4.png";
 import Step5 from "../../../assets/imgs/job-placement/step5.png";
 
 import Step1_1 from "../../../assets/imgs/job-placement/step1_1.png";
+import Step1_2 from "../../../assets/imgs/job-placement/step1_1.png";
+import Step1_3 from "../../../assets/imgs/job-placement/step1_1.png";
+import Step1_4 from "../../../assets/imgs/job-placement/step1_1.png";
+import Step1_5 from "../../../assets/imgs/job-placement/step1_1.png";
+
 
 export const DATA = [
   {
@@ -13,18 +18,18 @@ export const DATA = [
   },
   {
     imgURL: Step2,
-    imgURL2: Step1_1,
+    imgURL2: Step1_2,
   },
   {
     imgURL: Step3,
-    imgURL2: Step1_1,
+    imgURL2: Step1_3,
   },
   {
     imgURL: Step4,
-    imgURL2: Step1_1,
+    imgURL2: Step1_4,
   },
   {
     imgURL: Step5,
-    imgURL2: Step1_1,
+    imgURL2: Step1_5,
   },
 ];
